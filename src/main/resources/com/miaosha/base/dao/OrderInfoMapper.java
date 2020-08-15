e397245399d08fcc57391528f089f88423eb9f12
lxj <lixj_zj@163.com>
2020-08-15 11:43:38 +0800

    update com

---
+++ src/main/resources/com/miaosha/base/dao/OrderInfoMapper.java
@@ -0,0 +1,30 @@
+package com.miaosha.base.dao;
+
+import com.miaosha.base.vo.OrderInfo;
+import com.miaosha.base.vo.OrderInfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OrderInfoMapper {
+    int countByExample(OrderInfoExample example);
+
+    int deleteByExample(OrderInfoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(OrderInfo record);
+
+    int insertSelective(OrderInfo record);
+
+    List<OrderInfo> selectByExample(OrderInfoExample example);
+
+    OrderInfo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") OrderInfo record, @Param("example") OrderInfoExample example);
+
+    int updateByExample(@Param("record") OrderInfo record, @Param("example") OrderInfoExample example);
+
+    int updateByPrimaryKeySelective(OrderInfo record);
+
+    int updateByPrimaryKey(OrderInfo record);
+}
